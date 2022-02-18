260337571d2f0540484e5c208bb98397e01f3897
AliasNotFound <97630277+AliasNotFound@users.noreply.github.com>
2022-02-18 12:59:53 -0600

    Updated __init__.py to include more DB tables
    Added the following tables and their respective columns:
    users
    printers
    job_files
    job_history

--- db/__init__.py
+++ db/__init__.py
@@ -5,14 +5,26 @@ cur = con.cursor()
 # db initialization stuff
 # Create table
 cur.execute('''CREATE TABLE IF NOT EXISTS users
-               (date text, trans text, symbol text, qty real, price real)''')
+               (name TEXT, email TEXT, password TEXT, role BOOLEAN, quota INT, login_provider TEXT)''')
+
+
+cur.execute('''CREATE TABLE IF NOT EXISTS printers
+               (name TEXT, ip TEXT, camera TEXT, type TEXT, queue TEXT, uid TEXT)''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS job_files
+               (file_uid TEXT, filepath TEXT, filament_length INT)''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS job_history
+               (time_started DEFAULT CURRENT_TIMESTAMP, time_finished TIMESTAMP, status TEXT, jfid TEXT, pid TEXT)''')
+
 
 # Insert a row of data
-cur.execute("INSERT INTO users VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+cur.execute('''INSERT INTO users VALUES ('JASON', 'Jason@gmail.com', '######', 1, 10000, 'gmail')''')
+cur.execute('''INSERT INTO users VALUES ('JOSEPH', 'Joseph@hotmail.com', '######', 0, 5000, 'username')''')
 
 # Save (commit) the changes
 con.commit()
 
 def getUsers():
-    users = cur.execute("SELECT * FROM users").fetchall()
+    users = cur.execute("SELECT ROWID, * FROM users").fetchall()
     return users
