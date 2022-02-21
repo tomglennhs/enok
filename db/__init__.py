89b64d6fd2eefc025ddb9108f623664e59f439c0
Jason Antwi-Appah <hey@jasonaa.me>
2022-02-20 19:33:52 -0600

    chore: rename columns, switch filament length to float

--- db/__init__.py
+++ db/__init__.py
@@ -12,16 +12,18 @@ cur.execute('''CREATE TABLE IF NOT EXISTS printers
 
 
 cur.execute('''CREATE TABLE IF NOT EXISTS job_files
-               (id integer primary key, filepath TEXT, filament_length INT, uid int, FOREIGN KEY (uid) REFERENCES users(id))''')
+               (id integer primary key, filepath TEXT, filament_length REAL, user_id int, FOREIGN KEY (user_id) REFERENCES users(id))''')
 
 cur.execute('''CREATE TABLE IF NOT EXISTS job_history
-               (id integer primary key, time_started DEFAULT CURRENT_TIMESTAMP, time_finished TIMESTAMP, status TEXT, jfid int, pid int, FOREIGN KEY (jfid) REFERENCES job_files(id), FOREIGN KEY (pid) REFERENCES printers(id))''')
+               (id integer primary key, time_started DEFAULT CURRENT_TIMESTAMP, time_finished TIMESTAMP, status TEXT, job_file_id int, printer_id int, FOREIGN KEY (job_file_id) REFERENCES job_files(id), FOREIGN KEY (printer_id) REFERENCES printers(id))''')
+
 # Insert a row of data
 cur.execute('''INSERT INTO users (name, email, password, role, quota, login_provider) VALUES ('JASON', 'Jason@gmail.com', '######', 1, 10000, 'gmail')''')
 cur.execute('''INSERT INTO users (name, email, password, role, quota, login_provider) VALUES ('JOSEPH', 'Joseph@hotmail.com', '######', 0, 5000, 'username')''')
 
 # Save (commit) the changes
 con.commit()
+
 
 def getUsers():
     users = cur.execute("SELECT * FROM users").fetchall()
