c6e912acb547eded701d642f67daeab50c56b2bb
Jason Antwi-Appah <hey@jasonaa.me>
2022-02-25 12:45:26 -0600

    chore: rename routes

--- routes/jobs.py
+++ routes/jobs.py
@@ -3,18 +3,18 @@ from fastapi import APIRouter, Query, UploadFile
 router = APIRouter(prefix="/jobs", tags=["jobs"])
 
 
-@router.get("/jobs")
+@router.get("/")
 def list_jobs(all: bool = Query(False, description="List all uploaded jobs. Only for admins.")):
     return
 
 
-@router.post("/jobs")
+@router.post("/")
 def upload_job(file: UploadFile):
     return
 
 
 @router.get("/jobs/{id}")
-def upload_job(id: str):
+def get_job(id: str):
     return
 
 
