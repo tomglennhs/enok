1ce80135d9145a90fa3c96902975d8a41822b5fc
Jason Antwi-Appah <hey@jasonaa.me>
2022-02-25 12:58:16 -0600

    ye

--- main.py
+++ main.py
@@ -21,39 +21,11 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.on_event("shutdown")
-def shutdown_event():
-    db.con.close()
-
-
-@app.on_event("startup")
-def startup_event():
-    # StatusManager = sm.StatusManager
-    # print(StatusManager)
-    pass
-  
 # WIP
 @app.on_event("startup")
 def startup_event():
     state = sm.StatusManager()
 
-@app.get("/dev/store")
-@dev_only
-def read_store():
-    return store.getAll()
-
-
-@app.get("/users")
-def read_root():
-    users = db.get_table_data("users")
-    return {"Hello": users}
-
-#TODO make thise take in information from the front end JS scripts instead of being defined inside the function definition.
-@app.get("/upload_file")
-def upload_file(gcode = "file3.gcode", id = 1):
-    queue = sm.StatusManager.on_file_upload(gcode, db.get_printer_param(id, 'queue'))
-    db.set_queue(queue, id)
-    return queue
 
 @app.on_event("shutdown")
 def shutdown_event():
--- routes/jobs.py
+++ routes/jobs.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Query, UploadFile
-
+import db
+import StatusManager as sm
 router = APIRouter(prefix="/jobs", tags=["jobs"])
 
 
@@ -21,3 +22,10 @@ def get_job(id: str):
 @router.delete("/{id}")
 def delete_job(id: str):
     return
+
+#TODO make thise take in information from the front end JS scripts instead of being defined inside the function definition.
+@router.get("/upload_file")
+def upload_file(gcode = "file3.gcode", id = 1):
+    queue = sm.StatusManager.on_file_upload(gcode, db.get_printer_param(id, 'queue'))
+    db.set_queue(queue, id)
+    return queue
